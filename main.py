df8ab6649d7ea4d0e7042e802550250a2d543c7a
GhouI <flareonverm@outlook.com>
2023-10-18 13:35:05 +0100

    Update main.py
    -Converted to json rather to csv
    -Added more endpoints to hit,

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
-import pandas as pd
+import json
 import time
 import os
 
 # Credentials and API endpoint
-client_id = os.getenv('client_id')
+client_id = os.getenv('clientid')
 client_secret = os.getenv('client_secret')
 api_base_url = "https://sandbox.oizom.com/v1"
 token_url = f"{api_base_url}/oauth2/token"
@@ -23,17 +23,33 @@ token_payload = {
 token_response = session.post(token_url, json=token_payload)
 
 if token_response.status_code == 200:
-    access_token = token_response.json()["access_token"]
+  access_token = token_response.json()["access_token"]
 else:
-    print(f"Token request failed with status code {token_response.status_code}")
-    exit()
+  print(f"Token request failed with status code {token_response.status_code}")
+  exit()
 
-# Initialize an empty DataFrame to store all sensor data
-combined_data = pd.DataFrame()
+# Initialize an empty list to store all sensor data
+combined_data = [{"current": [], "daily": [], "hourly": []}]
 
 # Fetch and concatenate data for each sensor with rate limiting and retries
 for device_id in sensor_device_ids:
-    sensor_data_url = f"{api_base_url}/data/cur/{device_id}"
+  # List of endpoints to fetch data from
+  data_endpoints = [{
+      "url": f"{api_base_url}/data/cur/{device_id}",
+      "type": "current",
+      "num": 0,
+  }, {
+      "url": f"{api_base_url}/data/daily/{device_id}",
+      "type": "daily",
+      "num": 1,
+  }, {
+      "url": f"{api_base_url}/data/hourly/{device_id}",
+      "type": "hourly",
+      "num": 2,
+  }]
+
+  for endpoint in data_endpoints:
+    sensor_data_url = endpoint["url"]
     headers = {
         "Authorization": f"Bearer {access_token}",
         "ClientId": client_id,
@@ -42,26 +58,38 @@ for device_id in sensor_device_ids:
 
     retries = 0
     while retries < 3:  # Retry up to 3 times if rate limiting occurs
-        response = session.get(sensor_data_url, headers=headers)
+      response = session.get(sensor_data_url, headers=headers)
 
-        if response.status_code == 200:
-            data = response.json()
-            df = pd.DataFrame(data)
-            combined_data = combined_data._append(df, ignore_index=True)
-            print(f"Data for sensor {device_id} fetched and added to the combined data.")
-            break
-        elif response.status_code == 429:
-            # Rate limited, wait and retry
-            print(f"Rate limited for sensor {device_id}. Retrying...")
-            time.sleep(5)  # Wait for 5 seconds before retrying
-            retries += 1
-        else:
-            print(f"Data request for sensor {device_id} failed with status code {response.status_code}")
-            break
+      if response.status_code == 200:
+        data = response.json()
+        if (endpoint["type"] == "current"):
+          combined_data[0]["current"].append(data)
+        elif (endpoint["type"] == "daily"):
+          combined_data[0]["daily"].append(data)
+        elif (endpoint["type"] == "hourly"):
+          combined_data[0]["hourly"].append(data)
+        print(
+            f"Data for sensor {device_id} ({endpoint['type']}) fetched and added to the combined data."
+        )
+        break
+      elif response.status_code == 429:
+        # Rate limited, wait and retry
+        print(
+            f"Rate limited for sensor {device_id} ({endpoint['type']}). Retrying..."
+        )
+        time.sleep(5)  # Wait for 5 seconds before retrying
+        retries += 1
+      else:
+        print(
+            f"Data request for sensor {device_id} ({endpoint['type']}) failed with status code {response.status_code}"
+        )
+        break
 
-# Save the combined data to a single CSV file
-combined_data.to_csv("all_sensor_data.csv", index=False)
-print("Combined data for all sensors saved to all_sensor_data.csv")
+# Save the combined data to a single JSON file
+with open("all_sensor_data.json", "w") as json_file:
+  json.dump(combined_data, json_file, indent=4)
+
+print("Combined data for all sensors saved to all_sensor_data.json")
 
 # Close the session
 session.close()
